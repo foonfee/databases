baaec9fe977cd05af5740d6bd20ef1f4397f8258
Piotr Gorczewski <pgorczewski99@hotmail.com>
2018-09-23 17:00:38 +0200

    v1.0

---
+++ do it car repair/do_it_cr.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 12 Wrz 2018, 17:11
+-- Wersja serwera: 10.1.30-MariaDB
+-- Wersja PHP: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `do_it_cr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `faktury`
+--
+
+CREATE TABLE `faktury` (
+  `id` int(11) NOT NULL,
+  `id_pracownik` int(11) NOT NULL,
+  `id_klient` int(11) NOT NULL,
+  `sp_platnosci` int(11) NOT NULL,
+  `data_wystawienia` date NOT NULL,
+  `data_sprzedazy` date NOT NULL,
+  `nr` varchar(10) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `ulica` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `miasto` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `kod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pracownicy`
+--
+
+CREATE TABLE `pracownicy` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `login` varchar(11) COLLATE utf8_polish_ci NOT NULL,
+  `haslo` varchar(32) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uslugi`
+--
+
+CREATE TABLE `uslugi` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(200) COLLATE utf8_polish_ci NOT NULL,
+  `cena` decimal(9,2) NOT NULL,
+  `grupa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uslugi_faktury`
+--
+
+CREATE TABLE `uslugi_faktury` (
+  `id` int(11) NOT NULL,
+  `id_faktury` int(11) NOT NULL,
+  `id_uslugi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `faktury`
+--
+ALTER TABLE `faktury`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pracownik` (`id_pracownik`),
+  ADD KEY `id_klient` (`id_klient`),
+  ADD KEY `sp_platnosci` (`sp_platnosci`);
+
+--
+-- Indexes for table `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `uslugi`
+--
+ALTER TABLE `uslugi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `uslugi_faktury`
+--
+ALTER TABLE `uslugi_faktury`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_faktury` (`id_faktury`),
+  ADD KEY `id_uslugi` (`id_uslugi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `faktury`
+--
+ALTER TABLE `faktury`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `uslugi`
+--
+ALTER TABLE `uslugi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `uslugi_faktury`
+--
+ALTER TABLE `uslugi_faktury`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
